76535437dbb2fd75aeda0ffcb46bed8a3e17a745
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-03-18 18:33:35 +0530

    edit todo

--- 09-todos-web-ui/src/components/TodoInput.jsx
+++ 09-todos-web-ui/src/components/TodoInput.jsx
@@ -1,19 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import TodosContext from "../contexts/TodosContext";
 
 
-function TodoInput() {
+function TodoInput({ value = "", onEnter, onBlur }) {
     const { dispatch } = useContext(TodosContext);
+    const [inputValue, setInputValue] = useState(value);
     const handleKeyUp = (event) => {
-        if (event.key !== "Enter") return;
-        const text = event.target.value.trim();
-        if (text) {
-            dispatch({ type: "ADD_TODO", title: text });
+        if (event.key === "Escape") {
+            setInputValue("");
+            if (onBlur) {
+                onBlur();
+            }
+            return;
         }
-        event.target.value = "";
+        if (event.key !== "Enter") return;
+        if (inputValue.trim() !== "") {
+            if (onEnter) {
+                onEnter(inputValue);
+            } else {
+                dispatch({ type: "ADD_TODO", title: inputValue });
+            }
+            setInputValue("");
+        }
     }
     return (
-        <input onKeyUp={handleKeyUp} className="new-todo" placeholder="What needs to be done?" autoFocus />
+        <input onKeyUp={handleKeyUp}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onBlur={onBlur}
+            className="new-todo"
+            placeholder="What needs to be done?" autoFocus />
     )
 }
 
--- 09-todos-web-ui/src/components/TodoItem.jsx
+++ 09-todos-web-ui/src/components/TodoItem.jsx
@@ -1,14 +1,46 @@
+import { useContext, useState } from "react";
+import TodosContext from "../contexts/TodosContext";
+import TodoInput from "./TodoInput";
 
 
 
 function TodoItem({ todo }) {
+
+    const { dispatch } = useContext(TodosContext);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleDelete = () => {
+        dispatch({ type: "DELETE_TODO", id: todo.id });
+    }
+    const handleChange = (e) => {
+        dispatch({ type: "TOGGLE_TODO", id: todo.id });
+    }
+
+    const renderViewOrInput = () => {
+        if (isEditing) {
+            return (
+                <TodoInput
+                    onEnter={(title) => {
+                        dispatch({ type: "EDIT_TODO", id: todo.id, title });
+                        setIsEditing(false);
+                    }}
+                    value={todo.title}
+                    onBlur={() => setIsEditing(false)}
+                />
+            )
+        } else {
+            return (
+                <div className="view" onDoubleClick={() => setIsEditing(true)}>
+                    <input checked={todo.completed} onChange={handleChange} className="toggle" type="checkbox" />
+                    <label>{todo.title}</label>
+                    <button onClick={handleDelete} className="destroy"></button>
+                </div>
+            )
+        }
+    }
     return (
-        <li>
-            <div className="view">
-                <input className="toggle" type="checkbox" />
-                <label>{todo.title}</label>
-                <button className="destroy"></button>
-            </div>
+        <li className={todo.completed ? "completed" : ""}>
+            {renderViewOrInput()}
         </li>
     )
 }
